63ed19089112527bb7acc8dcf629d0c8e93016a2
Kim Jahn <kim@maisspace.org>
2016-04-05 15:53:04 +0200

    add cwd and user to exec
    fixes strage behaviour with manual puppet runs

--- manifests/rubygems.pp
+++ manifests/rubygems.pp
@@ -8,6 +8,8 @@ define rbenv::rubygems (
   exec { "update_rubygems_${user}_${version}":
     command => "${home}/.rbenv/shims/gem update --system ${version}",
     unless  => "${home}/.rbenv/shims/gem -v | /bin/grep ${version}",
+    user    => $user,
+    cwd     => $home,
     require => Exec["rbenv::compile ${user} ${ruby}"],
   }
 
