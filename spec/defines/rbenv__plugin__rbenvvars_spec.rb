f2f3f50d7274a9b87c47c2a3f1b33bcf433d28bd
Moritz Kraus <moritz.kraus@makandra.de>
2025-03-04 17:26:09 +0100

    fix tests

--- spec/defines/rbenv__plugin__rbenvvars_spec.rb
+++ spec/defines/rbenv__plugin__rbenvvars_spec.rb
@@ -4,6 +4,15 @@ describe 'rbenv::plugin::rbenvvars', :type => :define do
   let(:user)      { 'tester' }
   let(:title)     { user }
 
+  let(:pre_condition){
+    <<~PP
+      rbenv::install { "#{user}":
+        group => '',
+        home  => '/project'
+      }
+    PP
+  }
+
   it {
     should contain_rbenv__plugin("rbenv::plugin::rbenvvars::#{user}").with(
       :plugin_name => 'rbenv-vars',
--- spec/defines/rbenv__plugin__rubybuild_spec.rb
+++ spec/defines/rbenv__plugin__rubybuild_spec.rb
@@ -4,6 +4,15 @@ describe 'rbenv::plugin::rubybuild', :type => :define do
   let(:user)      { 'tester' }
   let(:title)     { user }
 
+  let(:pre_condition){
+    <<~PP
+      rbenv::install { "#{user}":
+        group => '',
+        home  => '/project'
+      }
+    PP
+  }
+
   it {
     should contain_rbenv__plugin("rbenv::plugin::rubybuild::#{user}").with(
       :plugin_name => 'ruby-build',
