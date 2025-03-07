25176150eb18596cbc8e6dc516ef887587099b7f
Moritz Kraus <moritz.kraus@makandra.de>
2025-03-07 15:49:48 +0100

    feat: use hiera to define packages

--- spec/classes/dependencies_spec.rb
+++ spec/classes/dependencies_spec.rb
@@ -5,6 +5,7 @@ require 'spec_helper'
 describe 'rbenv::dependencies' do
   on_supported_os.each do |os, os_facts|
     let(:facts) { os_facts }
+    let(:hiera_config) { 'hiera.yaml' }
 
     context "on #{os}" do
       it { should compile }
