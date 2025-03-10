b380a610f43a68926de794cea717b2eed6d1f900
Moritz Kraus <moritz.kraus@makandra.de>
2025-03-10 15:26:53 +0100

    chore: run unit test on all supported distros

--- spec/defines/rbenv__definition_spec.rb
+++ spec/defines/rbenv__definition_spec.rb
@@ -1,53 +1,58 @@
 require 'spec_helper'
 
 describe 'rbenv::definition', :type => :define do
-  let(:user)         { 'tester' }
-  let(:ruby_version) { '1.9.3-p125' }
-  let(:title)        { "rbenv::definition::#{user}::#{ruby_version}" }
-  let(:dot_rbenv)    { "/home/#{user}/.rbenv" }
-  let(:target_path)  { "#{dot_rbenv}/plugins/ruby-build/share/ruby-build/#{ruby_version}" }
-  let(:params)       { {:user => user, :ruby => ruby_version, :source => definition} }
+  on_supported_os.each do |os, os_facts|
+    let(:user)         { 'tester' }
+    let(:ruby_version) { '1.9.3-p125' }
+    let(:title)        { "rbenv::definition::#{user}::#{ruby_version}" }
+    let(:dot_rbenv)    { "/home/#{user}/.rbenv" }
+    let(:target_path)  { "#{dot_rbenv}/plugins/ruby-build/share/ruby-build/#{ruby_version}" }
+    let(:params)       { {:user => user, :ruby => ruby_version, :source => definition} }
+    let(:facts)        { os_facts }
 
-  let(:pre_condition){
-    <<~PP
-      rbenv::install { "#{user}":
-        group => '',
-        home  => '/project'
-      }
-      rbenv::plugin {"ruby-build":
-        user   => '#{user}',
-        source => 'git://gist.com/ree',
-      }
-    PP
-  }
-
-  context 'puppet' do
-    let(:definition)   { 'puppet:///custom-definition' }
-    it 'copies the file to the right path' do
-      should contain_file("rbenv::definition-file #{user} #{ruby_version}").with(
-        :path => target_path,
-        :source  => definition
-      )
+    let(:pre_condition)  do
+      <<~PP
+        rbenv::install { "#{user}":
+          group => '',
+          home  => '/project'
+        }
+        rbenv::plugin {"ruby-build":
+          user   => '#{user}',
+          source => 'git://gist.com/ree',
+        }
+      PP
     end
-  end
 
-  context 'http' do
-    let(:definition) { 'http://gist.com/ree' }
-    it 'downloads file to the right path' do
-      should contain_exec("rbenv::definition-file #{user} #{ruby_version}").with(
-        :command => "wget #{definition} -O #{target_path}",
-        :creates => target_path
-      )
-    end
-  end
+    context "on #{os}" do
+      context 'puppet' do
+        let(:definition)   { 'puppet:///custom-definition' }
+        it 'copies the file to the right path' do
+          should contain_file("rbenv::definition-file #{user} #{ruby_version}").with(
+            :path => target_path,
+            :source  => definition
+          )
+        end
+      end
 
-  context 'https' do
-    let(:definition) { 'https://gist.com/ree' }
-    it 'downloads file to the right path' do
-      should contain_exec("rbenv::definition-file #{user} #{ruby_version}").with(
-        :command => "wget #{definition} -O #{target_path}",
-        :creates => target_path
-      )
+      context 'http' do
+        let(:definition) { 'http://gist.com/ree' }
+        it 'downloads file to the right path' do
+          should contain_exec("rbenv::definition-file #{user} #{ruby_version}").with(
+            :command => "wget #{definition} -O #{target_path}",
+            :creates => target_path
+          )
+        end
+      end
+
+      context 'https' do
+        let(:definition) { 'https://gist.com/ree' }
+        it 'downloads file to the right path' do
+          should contain_exec("rbenv::definition-file #{user} #{ruby_version}").with(
+            :command => "wget #{definition} -O #{target_path}",
+            :creates => target_path
+          )
+        end
+      end
     end
   end
 end
--- spec/defines/rbenv__plugin__rubybuild_spec.rb
+++ spec/defines/rbenv__plugin__rubybuild_spec.rb
@@ -1,23 +1,28 @@
 require 'spec_helper'
 
 describe 'rbenv::plugin::rubybuild', :type => :define do
-  let(:user)      { 'tester' }
-  let(:title)     { user }
+  on_supported_os.each do |os, os_facts|
+    let(:user)      { 'tester' }
+    let(:title)     { user }
+    let(:facts)     { os_facts }
 
-  let(:pre_condition){
-    <<~PP
-      rbenv::install { "#{user}":
-        group => '',
-        home  => '/project'
+    let(:pre_condition) do
+      <<~PP
+        rbenv::install { "#{user}":
+          group => '',
+          home  => '/project'
+        }
+      PP
+    end
+
+    context "on #{os}" do
+      it {
+        should contain_rbenv__plugin("rbenv::plugin::rubybuild::#{user}").with(
+          :plugin_name => 'ruby-build',
+          :source      => 'https://github.com/sstephenson/ruby-build.git',
+          :user        => user
+        )
       }
-    PP
-  }
-
-  it {
-    should contain_rbenv__plugin("rbenv::plugin::rubybuild::#{user}").with(
-      :plugin_name => 'ruby-build',
-      :source      => 'https://github.com/sstephenson/ruby-build.git',
-      :user        => user
-    )
-  }
+    end
+  end
 end
