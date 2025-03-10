26373fc962a39d3fd41b324a6dc52a33eb38eee4
Moritz Kraus <moritz.kraus@makandra.de>
2025-03-10 15:26:53 +0100

    chore: run unit test on all supported distros

--- spec/classes/dependencies_spec.rb
+++ spec/classes/dependencies_spec.rb
@@ -1,25 +1,15 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe 'rbenv::dependencies' do
-  let(:title) { 'rbenv::dependencies' }
+  on_supported_os.each do |os, os_facts|
+    let(:facts) { os_facts }
 
-  context 'Ubuntu' do
-    let(:facts) { {:osfamily => 'debian'} }
-    it { should include_class('rbenv::dependencies::ubuntu') }
-  end
-
-  context 'RedHat' do
-    let(:facts) { {:osfamily => 'redhat'} }
-    it { should include_class('rbenv::dependencies::centos') }
-  end
-
-  context 'Suse' do
-    let(:facts) { {:osfamily => 'suse'} }
-    it { should include_class('rbenv::dependencies::suse') }
-  end
-
-  context 'Amazon Linux' do
-    let(:facts) { {:osfamily => 'Linux'} }
-    it { should include_class('rbenv::dependencies::centos') }
+    context "on #{os}" do
+      it { should compile }
+      it { should contain_package('git') }
+      it { should contain_package('curl') }
+    end
   end
 end
--- spec/defines/rbenv__compile_spec.rb
+++ spec/defines/rbenv__compile_spec.rb
@@ -1,45 +1,49 @@
 require 'spec_helper'
 
 describe 'rbenv::compile', :type => :define do
-  let(:user)         { 'tester' }
-  let(:ruby_version) { '1.9.3-p125' }
-  let(:title)        { "rbenv::compile::#{user}::#{ruby_version}" }
-  let(:dot_rbenv)    { "/home/#{user}/.rbenv" }
-  let(:bundler)      { '1.3.4' }
-  let(:params)       { {:user => user, :ruby => ruby_version, :global => true, :bundler => bundler} }
+  on_supported_os.each do |os, os_facts|
+    let(:user)         { 'tester' }
+    let(:ruby_version) { '1.9.3-p125' }
+    let(:title)        { "rbenv::compile::#{user}::#{ruby_version}" }
+    let(:dot_rbenv)    { "/home/#{user}/.rbenv" }
+    let(:bundler)      { '1.3.4' }
+    let(:params)       { {:user => user, :ruby => ruby_version, :global => true, :bundler => bundler} }
+    let(:facts)        { os_facts }
 
+    let(:pre_condition)  do
+      <<~PP
+        rbenv::install { "#{user}":
+          group => '',
+          home  => '/project'
+        }
+      PP
+    end
 
-  let(:pre_condition){
-    <<~PP
-      rbenv::install { "#{user}":
-        group => '',
-        home  => '/project'
-      }
-    PP
-  }
+    context "on #{os}" do
+      it "installs ruby of the chosen version" do
+        should contain_exec("rbenv::compile #{user} #{ruby_version}").
+          with_command("rbenv install #{ruby_version} && touch #{dot_rbenv}/.rehash")
+      end
 
-  it "installs ruby of the chosen version" do
-    should contain_exec("rbenv::compile #{user} #{ruby_version}").
-      with_command("rbenv install #{ruby_version} && touch #{dot_rbenv}/.rehash")
-  end
+      it "issues a rehash command" do
+        should contain_exec("rbenv::rehash #{user} #{ruby_version}").
+          with_command("rbenv rehash && rm -f #{dot_rbenv}/.rehash")
+      end
 
-  it "issues a rehash command" do
-    should contain_exec("rbenv::rehash #{user} #{ruby_version}").
-      with_command("rbenv rehash && rm -f #{dot_rbenv}/.rehash")
-  end
+      it "sets the global ruby version for the specific user" do
+        should contain_file("rbenv::global #{user}").
+          with_content("#{ruby_version}\n").
+          with_require("Exec[rbenv::compile #{user} #{ruby_version}]")
+      end
 
-  it "sets the global ruby version for the specific user" do
-    should contain_file("rbenv::global #{user}").
-      with_content("#{ruby_version}\n").
-      with_require("Exec[rbenv::compile #{user} #{ruby_version}]")
-  end
+      it "installs ruby-build plugin from official repository" do
+        should contain_rbenv__plugin__rubybuild("rbenv::rubybuild::#{user}")
+      end
 
-  it "installs ruby-build plugin from official repository" do
-    should contain_rbenv__plugin__rubybuild("rbenv::rubybuild::#{user}")
-  end
-
-  it "installs bundler" do
-    should contain_rbenv__gem("rbenv::bundler #{user} #{ruby_version}").
-      with_ensure(bundler)
+      it "installs bundler" do
+        should contain_rbenv__gem("rbenv::bundler #{user} #{ruby_version}").
+          with_ensure(bundler)
+      end
+    end
   end
 end
