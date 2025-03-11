65f24024b5eff589a54c070be8e30c5799d92c30
Moritz Kraus <moritz.kraus@makandra.de>
2025-03-11 10:07:28 +0100

    fix: enable crb on RedHat to install libyaml-devel

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -4,5 +4,11 @@
 class rbenv::dependencies (
   Array $packages,
 ) {
+  if $facts['os']['family'] == 'RedHat' {
+    # this is required for installing libyaml-devel
+    exec { '/usr/bin/dnf config-manager --set-enabled crb':
+      unless => '/usr/bin/dnf repolist enabled | grep -q crb',
+    }
+  }
   stdlib::ensure_packages($packages)
 }
